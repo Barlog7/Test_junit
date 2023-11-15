7d92afd467bad6abe47bee23cd60c5657bc5ea03
Barlog7 <barlog7@yandex.ru>
2023-11-15 17:17:32 +0300

    simple test jUnit

---
+++ src/test/java/org/example/SomeClassTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeClassTest {
+
+    @Test
+    void testSum() {
+        var expected = 5;
+        var actual = SomeClass.sum(3, 2);
+        assertEquals(expected, actual);
+    }
+}
